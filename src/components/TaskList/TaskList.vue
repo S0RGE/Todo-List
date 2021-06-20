9190ee9f369168bc56b35342fb9464d7ed10f75f
Nifontov Vitaliy <Vitaliynif@gmail.com>
2021-06-20 15:24:39 +0700

    asd

--- src/components/TaskList/TaskList.vue
+++ src/components/TaskList/TaskList.vue
@@ -1,23 +1,32 @@
 <template>
-  <v-card>
-    <v-list>
-      <v-list-item-group v-model="model">
-        <v-list-item
-          v-for="(item, i) in items"
-          :key="i"
-        >
-          <v-list-item-icon>
-            <v-icon v-text="item.icon"></v-icon>
-          </v-list-item-icon>
-          <v-list-item-content>
-            <v-list-item-title v-text="item.text"></v-list-item-title>
-          </v-list-item-content>
-        </v-list-item>
-      </v-list-item-group>
-    </v-list>
+    <v-card>
+      <v-list>
+        <v-list-item-group v-model="model" max-height>
+          <v-list-item
+            v-for="(item, i) in items"
+            :key="i"
+          >
+            <v-list-item-icon>
+              <v-icon v-text="item.icon"></v-icon>
+            </v-list-item-icon>
+            <v-list-item-content>
+              <v-list-item-title v-text="item.text"></v-list-item-title>
+            </v-list-item-content>
+          </v-list-item>
+        </v-list-item-group>
+      </v-list>
+      <v-text-field
+      label="Add Task List"
+      class="mx-2"
+      v-model="taskList"
+      ></v-text-field>
     <v-card-actions>
-      <v-text-field label="Add Task List" width="90%"></v-text-field>
-      <v-btn dark rounded>Ok</v-btn>
+      <v-btn
+      @click="addTaskList()"
+      width="100%"
+      dark
+      color="deep-purple accent-4"
+      >Add List</v-btn>
     </v-card-actions>
   </v-card>
 </template>
@@ -44,7 +53,18 @@ export default {
           text: 'forth'
         }
       ],
-      model: 1
+      model: 1,
+      taskList: ''
+    }
+  },
+  methods: {
+    addTaskList () {
+      this.items.push({
+        icon: 'mdi-inbox',
+        text: this.taskList
+      })
+      // this.$store.dispatch('addTodoList', this.taskList)
+      this.taskList = ''
     }
   }
 }
--- src/views/Login.vue
+++ src/views/Login.vue
@@ -3,8 +3,8 @@
   v-model="tab"
   grow
   >
-    <v-tab href="#tab-1">Login</v-tab>
-    <v-tab href="#tab-2">Register</v-tab>
+    <v-tab href="#tab-1" @click="resetFields()">Login</v-tab>
+    <v-tab href="#tab-2" @click="resetFields()">Register</v-tab>
     <v-tabs-items
     v-model="tab"
     >
@@ -60,6 +60,7 @@
                 <v-text-field
                 v-model="email"
                 outlined
+                :rules="emailRules"
                 label="Email"
                 hide-details="auto"
                 ></v-text-field>
@@ -69,6 +70,7 @@
                 hide-details="auto"
                 v-model="password"
                 type="password"
+                :rules="passwordRules"
                 outlined
                 label="Password"
                 ></v-text-field>
@@ -78,6 +80,7 @@
                 hide-details="auto"
                 outlined
                 label="Name"
+                :rules="nameRules"
                 v-model="name"
                 ></v-text-field>
               </v-col>
@@ -108,11 +111,36 @@ export default {
     valid: false,
     email: '',
     password: '',
-    agreeTheStatement: '',
-    regDialog: false
+    agreeTheStatement: false,
+    passwordRules: [
+      v => !!v.trim() || 'Введите пароль',
+      v =>
+        /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])[a-zA-Z0-9]{8,}$/.test(v) ||
+      'Пароль должен содержать минимум 8 символов, 1 прописную букву, 1 строчную букву, 1 цифру'
+    ],
+    emailRules: [
+      v => !!v.trim() || 'Введите Вашу электронную почту',
+      v =>
+        /^(([^<>()\]\\.,;:\s@"]+(\.[^<>()\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(
+          v
+        ) || 'Введите правильную электронную почту',
+      v => v.length <= 100 || 'Слишком большая почта'
+    ],
+    nameRules: [
+      v => !!v.trim() || 'Введите Ваше имя',
+      v => v.length <= 35 || 'Слишком большое имя',
+      v =>
+        /^([a-zа-яё]+|\d+)$/i.test(v) ||
+      'Имя должно содержать только русский и латинский алфавит'
+    ]
   }),
   methods: {
+    resetFields () {
+      this.email = this.password = this.name = ''
+      this.agreeTheStatement = false
+    },
     async login () {
+      // if (this.email.trim() === '' || this.password.trim() === '') return
       const userData = {
         email: this.email,
         password: this.password
@@ -124,6 +152,7 @@ export default {
       } catch (e) {}
     },
     async register () {
+      // if (this.name.trim() === '' || this.password.trim() === '' || this.agreeTheStatement || this.email.trim() === '') return
       const newUser = {
         email: this.email,
         password: this.password,
