24a98783d46bb9a04af06e52ac3120dc85e678ce
Nifontov Vitaliy <Vitaliynif@gmail.com>
2021-06-28 14:51:13 +0700

    edit gh-pages-deploy

--- scripts/gh-pages-deploy.js
+++ scripts/gh-pages-deploy.js
@@ -4,8 +4,8 @@ const fs = require("fs");
 
 (async () => {
   try {    
-    // await execa("git", ["push", "origin", "--delete", "gh-pages"]);
-    // await execa("git", ["branch", "-D", "gh-pages"]);
+    await execa("git", ["push", "origin", "--delete", "gh-pages"]);
+    await execa("git", ["branch", "-D", "gh-pages"]);
     await execa("git", ["checkout", "--orphan", "gh-pages"]);
     console.log("Building...");
     await execa("npm", ["run", "build"]);
@@ -19,6 +19,8 @@ const fs = require("fs");
     await execa("git", ["checkout", "-f", "master"]);
     await execa("git", ["branch", "-D", "gh-pages"]);
     console.log("Successfully deployed");
+    await execa("git", ["checkout", "main"]);
+    console.log("Returned to main");
   } catch (e) {
     console.log(e.message);
     process.exit(1);
