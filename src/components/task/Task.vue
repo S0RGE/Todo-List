591fcc9dff548b628af813d31893d565fed11d94
Nifontov Vitaliy <Vitaliynif@gmail.com>
2021-06-28 17:44:06 +0700

    refactor some code

--- src/components/task/Task.vue
+++ src/components/task/Task.vue
@@ -1,28 +1,16 @@
 <template>
-  <v-card
-    max-width="100%"
-    class="mx-auto"
-  >
-    <v-app-bar
-      dark
-      color="deep-purple accent-4"
-    >
+  <v-card max-width="100%" class="mx-auto">
+    <v-app-bar dark color="deep-purple accent-4">
       <v-app-bar-nav-icon @click="$emit('toggleTaskList')"></v-app-bar-nav-icon>
-      <v-toolbar-title v-if="$vuetify.breakpoint.name !== 'xs'">{{ $route.params.listName.toUpperCase() }} - Task List</v-toolbar-title>
+      <v-toolbar-title v-if="$vuetify.breakpoint.name !== 'xs'"
+        >{{ $route.params.listName.toUpperCase() }} - Task List</v-toolbar-title
+      >
       <v-spacer></v-spacer>
-       <v-text-field
-            class="mt-4"
-            v-model="taskFind"
-            label="Find Task"
-          >
-          <v-icon
-            v-if="taskFind"
-            slot="append"
-            @click="taskFind = ''"
-            >
-            mdi-close
-            </v-icon>
-          </v-text-field>
+      <v-text-field class="mt-4" v-model="taskFind" label="Find Task">
+        <v-icon v-if="taskFind" slot="append" @click="taskFind = ''">
+          mdi-close
+        </v-icon>
+      </v-text-field>
       <v-btn icon>
         <v-icon>mdi-magnify</v-icon>
       </v-btn>
@@ -38,84 +26,95 @@
           cols="10"
           class="mx-auto"
         >
-          <v-card
-            dark
-            :color="task.isDone? 'grey' : 'deep-purple accent-4'"
-          >
-            <div class="d-flex flex-no-wrap justify-space-between">
-              <div>
-                <v-card-title
-                  class="text-h5"
-                  :class="{ priorityItem: task.priority }"
-                  v-text="task.title"
-                >{{ task.title }}</v-card-title>
-                <v-card-subtitle v-text="task.description"></v-card-subtitle>
-                <v-card-actions>
-                  <v-btn
-                    class="ml-2 mt-5"
-                    outlined
-                    rounded
-                    small
-                    @click="editTask(i)"
-                  >
-                    Edit task
-                  </v-btn>
-                  <v-btn
-                    class="ml-2 mt-5"
-                    outlined
-                    rounded
-                    small
-                    @click="deleteTask(task)"
-                  >
-                    Delete Task
-                  </v-btn>
-                  <v-btn
-                    class="ml-2 mt-5"
-                    outlined
-                    rounded
-                    small
-                    @click="doneTask(task)"
-                  >
-                    Done Task
-                  </v-btn>
-                </v-card-actions>
-              </div>
-              <v-avatar
-                v-if="$vuetify.breakpoint.name !== 'xs' && $vuetify.breakpoint.name !== 'sm'"
-                class="ma-3"
-                size="125"
-                tile
+          <v-hover>
+            <template v-slot:default="{ hover }">
+              <v-card
+                dark
+                :color="task.isDone ? 'grey' : 'deep-purple accent-4'"
+                :elevation="hover ? 24 : 6"
               >
-                <v-img @click="editPicture()" style="cursor: pointer" :src="task.src"></v-img>
-              </v-avatar>
-            </div>
-          </v-card>
+                <div class="d-flex flex-no-wrap justify-space-between">
+                  <div>
+                    <v-card-title
+                      class="text-h5"
+                      :class="{ priorityItem: task.priority }"
+                      v-text="task.title"
+                    ></v-card-title>
+                    <v-card-subtitle
+                      v-text="task.description"
+                    ></v-card-subtitle>
+                    <v-card-actions>
+                      <v-btn
+                        class="ml-2 mt-5"
+                        outlined
+                        rounded
+                        small
+                        @click="editTask(i)"
+                      >
+                        Edit task
+                      </v-btn>
+                      <v-btn
+                        class="ml-2 mt-5"
+                        outlined
+                        rounded
+                        small
+                        @click="deleteTask(task)"
+                      >
+                        Delete Task
+                      </v-btn>
+                      <v-btn
+                        class="ml-2 mt-5"
+                        outlined
+                        rounded
+                        small
+                        @click="doneTask(task)"
+                      >
+                        Done Task
+                      </v-btn>
+                    </v-card-actions>
+                  </div>
+                  <v-avatar
+                    v-if="
+                      $vuetify.breakpoint.name !== 'xs' &&
+                        $vuetify.breakpoint.name !== 'sm'
+                    "
+                    class="ma-3"
+                    size="125"
+                    tile
+                  >
+                    <v-img
+                      @click="editPicture()"
+                      style="cursor: pointer"
+                      :src="task.src"
+                    ></v-img>
+                  </v-avatar>
+                </div>
+              </v-card>
+            </template>
+          </v-hover>
         </v-col>
         <v-col cols="10" class="mx-auto">
-        <v-btn dark
-          @click="dialog = !dialog"
-             color="deep-purple accent-4" width="100%">Add task</v-btn>
+          <v-btn
+            dark
+            @click="dialog = !dialog"
+            color="deep-purple accent-4"
+            width="100%"
+            >Add task</v-btn
+          >
         </v-col>
       </v-row>
     </v-container>
     <v-col cols="auto">
       <v-dialog
-      v-model="dialog"
+        v-model="dialog"
         transition="dialog-bottom-transition"
         max-width="600"
       >
         <template>
           <v-card>
-            <v-toolbar
-              color="deep-purple accent-4"
-              dark
-            >Edit Task</v-toolbar>
+            <v-toolbar color="deep-purple accent-4" dark>Edit Task</v-toolbar>
             <v-card-text>
-              <TaskEdit
-                :id="id"
-                @closeForm="closeForm()"
-                @addTask="addTask"
-              />
+              <TaskEdit :id="id" @closeForm="closeForm()" @addTask="addTask" />
             </v-card-text>
           </v-card>
         </template>
@@ -165,17 +164,25 @@ export default {
     addTask (task) {
       const newTask = {
         color: task.color || '#952175',
-        src: task.src || 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQlUGwpR2u_hnvnP0q2r-F8UoAYsTaWDz2aSg&usqp=CAU',
+        src:
+          task.src ||
+          'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQlUGwpR2u_hnvnP0q2r-F8UoAYsTaWDz2aSg&usqp=CAU',
         title: task.taskName,
         description: task.taskDescription,
-        created: task.created || new Date().toJSON().slice(0, 10).replace(/-/g, '/'),
+        created:
+          task.created ||
+          new Date()
+            .toJSON()
+            .slice(0, 10)
+            .replace(/-/g, '/'),
         priority: task.priority,
-        isDone: task.isDone || false, // what is done?
+        isDone: task.isDone || false, // catch is done
         list: this.$route.params.listName
       }
       if (this.id !== null) {
         newTask.lastName = this.tasks[this.id].title
-        this.$store.dispatch('editTaskAsync', newTask) // Add previous task name to change
+        console.log('addTask', this.tasks[this.id].title)
+        this.$store.dispatch('editTaskAsync', newTask)
         this.tasks.splice(this.id, 1, newTask)
       } else {
         this.$store.dispatch('addTodoTask', newTask)
@@ -185,7 +192,9 @@ export default {
   },
   computed: {
     sortedTasks () {
-      return this.tasks?.filter(item => item.list === this.$route.params.listName)
+      return this.tasks?.filter(
+        item => item.list === this.$route.params.listName
+      )
     }
   },
   async mounted () {
